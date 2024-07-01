8ec3b60cd47907bd3e403dfe389e47ec4a9dbd61
Marc-Jalkh <marcjalkh.mej@gmail.com>
2024-07-01 15:07:34 +0300

    used top app bar

--- Components/src/main/java/com/tomorrow/components/headers/PageHeaderLayout.kt
+++ Components/src/main/java/com/tomorrow/components/headers/PageHeaderLayout.kt
@@ -5,11 +5,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.mutableStateOf
@@ -22,6 +27,7 @@ import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
@@ -32,6 +38,7 @@ import com.tomorrow.components.buttons.BackButton
  * if no function was passed for onBackPress
  * the button will not be displayed
  * */
+@ExperimentalMaterial3Api
 @Composable
 fun PageHeaderLayout(
     modifier: Modifier = Modifier,
@@ -41,49 +48,67 @@ fun PageHeaderLayout(
     contentPadding: PaddingValues = PaddingValues(horizontal = 16.dp),
     titleStyle: TextStyle = MaterialTheme.typography.headlineSmall,
     subtitleStyle: TextStyle = MaterialTheme.typography.titleMedium.copy(color = MaterialTheme.colorScheme.secondary),
+    colors: TopAppBarColors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Transparent),
     rightCornerContent: @Composable () -> Unit = {},
     content: @Composable () -> Unit,
 ) = Column(modifier.fillMaxHeight()) {
     CompositionLocalProvider(
         LocalDensity provides Density(LocalDensity.current.density, 1f)
     ) {
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp)
-                .padding(top = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(20.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            onBackPress?.let {
-                BackButton(onClick = { it() })
-            }
-
-            Column {
-                title?.let {
-                    Row(
-                        Modifier,
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(text = it, style = titleStyle)
+        TopAppBar(
+            title = {
+                Column(Modifier) {
+                    title?.let {
+                        Row(
+                            Modifier,
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                        ) {
+                            Text(text = it, style = titleStyle, maxLines = 1)
+                        }
+                    }
+                    subtitle?.let {
+                        Text(
+                            modifier = Modifier.padding(top = 7.dp),
+                            text = it,
+                            style = subtitleStyle,
+                            maxLines = 1
+                        )
                     }
                 }
-                subtitle?.let {
-                    Text(
-                        modifier = Modifier.padding(top = 7.dp),
-                        text = it,
-                        style = subtitleStyle
-                    )
+            },
+            modifier = Modifier,
+            navigationIcon = {
+                onBackPress?.let {
+                    BackButton(onClick = { it() })
                 }
-            }
-            Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.End) {
+            },
+            actions = {
                 rightCornerContent()
-            }
-        }
+            },
+            colors = colors
+        )
     }
 
     Column(modifier = Modifier.padding(contentPadding)) {
         content()
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun PageHeaderLayoutPreview() {
+    PageHeaderLayout(
+        title = "Title",
+        subtitle = "Subtitle , Subtitle Subtitle SubtitleSubtitle Subtitle Subtitle Subtitle v Subtitle v ",
+        onBackPress = {},
+        rightCornerContent = {
+            Text("Right corner content")
+        },
+        content = {
+            AppSeparator(Modifier.fillMaxSize())
+        }
+    )
+
 }
--- Components/src/main/java/com/tomorrow/components/others/GeneralError.kt
+++ Components/src/main/java/com/tomorrow/components/others/GeneralError.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -27,6 +28,7 @@ import androidx.compose.ui.unit.dp
 import com.tomorrow.components.R
 import com.tomorrow.components.headers.PageHeaderLayout
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GeneralError(
     modifier: Modifier = Modifier,
